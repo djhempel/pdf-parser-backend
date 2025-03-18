2432a0bdf33ca45827de4f38b990c094920fc11b
djhempel <48452338+djhempel@users.noreply.github.com>
2025-03-18 19:29:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.post('/extract-text', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
